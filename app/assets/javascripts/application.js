5e5d6757d8b6b8ecc03b543da9079cc354c9709b
Tomonori-T <noritt3397@gmail.com>
2021-05-17 11:25:38 +0000

    [Add] To application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,25 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
 //= require_tree .
+$(function() {
+  $(document).on('turbolinks:load', () => {
+    $('#user_postal_code').jpostal({
+      postcode : [
+        '#user_postal_code'
+      ],
+      address: {
+        "#user_prefecture_code": "%3", // # 都道府県が入力される
+        "#user_city"           : "%4%5", // # 市区町村と町域が入力される
+        "#user_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+      }
+    });
+  });
+});
