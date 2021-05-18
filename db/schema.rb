3d1a571ea0f6f620175a8b336661fb44a9c02d3f
Tomonori-T <80507288+Tomonori-T@users.noreply.github.com>
2021-05-18 19:16:10 +0900

    Update schema.rb
    コンフリクト対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_074913) do
+ActiveRecord::Schema.define(version: 2021_05_18_051104) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
