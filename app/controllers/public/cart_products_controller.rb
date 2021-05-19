d2c0e44e49d11d9480675d4bb5abf45af6989059
Tomonori-T <noritt3397@gmail.com>
2021-05-19 01:46:31 +0000

    [Add] cart

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,3 +1,14 @@
 class CartProductsController < ApplicationController
   #test1
+  def show
+  end
+  def add_item
+  end
+  def update_item
+  end
+  def delete_item
+  end
+  private
+  def setup_cart_item!
+  end
 end
