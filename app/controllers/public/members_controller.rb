f1097c7ee50f44b56d5ff4784ffb5dba5319fcc2
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-05-20 12:28:41 +0000

    [Update]ユーザー編集機能

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,19 +1,30 @@
 class Public::MembersController < ApplicationController
   def show
-    @member= Member.find(params[:id])
+    @member = Member.find(params[:id])
+  end
+
+  def withdrawal
+  end
+
+  def hide
+    @member = Member.find(params[:id])
+    @member.update(withdrawal_flag: true)
+    reset_session
+    flash[:notice] = "またのご利用をお待ちしております。"
+    redirect_to top_path
   end
 
   def edit
-    @member = current_member
+    @member = Member.find(params[:id])
   end
 
   def update
-    @member = current_member
+    @member = Member.find(params[:id])
     if @member.update(member_params)
       flash[:success] = "登録情報を変更しました"
-      redirect_to members_path
+      redirect_to public_member_path(@member.id)
     else
-      render :edit and return
+      render :edit
     end
   end
 
@@ -21,4 +32,5 @@ class Public::MembersController < ApplicationController
   def member_params
     params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number)
   end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,12 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cart_product, dependent: :destroy
+  has_many :orders
+  has_many :received, dependent: :destroy
+
+  def active_for_authenticcation?
+    super && (self.withdrawal_flag == false)
+  end
 end
