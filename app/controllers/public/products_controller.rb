ee16277f145b87c3320cd1a2b3243f5ed8259339
shota-hibino <yamoushou@gmail.com>
2021-05-21 02:38:19 +0000

    [Update]view and controller create

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,29 @@
-class ProductsController < ApplicationController
+class Customer::ProductsController < ApplicationController
+
+  before_action :authenticate_customer!, only: [:show]
+
+  def top
+    @products = Product.all.order(created_at: :asc)
+    @genres = Genre.all
+  end
+
+	def index
+    @genres = Genre.all
+    @products = Product.where(sale_status: true).page(params[:page]).per(8)
+	end
+
+	def show
+    @products = Product.all
+    @product = Product.find(params[:id])
+    @cart_item = CartItem.new
+	end
+
+  def about
+  end
+
+	private
+	def product_params
+		parmas.require(:product).permit(:image ,:name, :description, :price, :sale_status)
+	end
+
 end
