f81f379424fc6a87738f495a75b96db8c74b038a
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-05-22 09:00:33 +0000

    ユーザーの配送先機能の追加

--- app/models/received.rb
+++ app/models/received.rb
@@ -1,2 +1,13 @@
 class Received < ApplicationRecord
+  belongs_to :member
+
+  def order_address
+    self.postal_code + self.address + self.name
+  end
 end
+
+    #↓完成させたらschemaに追加
+    #t.integer "postal_code"
+    #t.string "prefecture_code"
+    #t.string "city"
+    #t.string "street"
