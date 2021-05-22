b11467d27efc8467069fd260847b0073cf265cef
yuma0313 <y.yuma0313@gmail.com>
2021-05-22 11:38:38 +0000

    genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,4 +1,6 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!, except: [:show]
+  
   def index
     @genres = Genre.all
     @genre = Genre.new
@@ -17,6 +19,12 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
+  def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
     redirect_to admin_genres_path
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: "admin@icloud.com",
+  email: 'admin@icloud.com',
   password: "aaaaaa"
 )
+
+Genre.create!(
+    name: "ケーキ"
+  )
+Genre.create!(
+    name: "プリン"
+  )
+Genre.create!(
+    name: "焼き菓子"
+  )
+Genre.create!(
+    name: "キャンディ"
+  )
+Genre.create!(
+    name: "チョコケーキ"
+  )
