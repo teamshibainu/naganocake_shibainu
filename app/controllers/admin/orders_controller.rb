5c11983e0d74abcb2c6f2e0c9b224decb11b420a
yuma0313 <y.yuma0313@gmail.com>
2021-05-22 08:57:11 +0000

    genre

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,20 @@
-class OrdersController < ApplicationController
+class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
+  
+  def edit
+    @order = Order.find(params[:id])
   end
 
   def update
+    @order = Order,find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
   end
 
   private
