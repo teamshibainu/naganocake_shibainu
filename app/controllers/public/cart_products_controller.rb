06aa9804d7fed760f24a8b96f2ab91872080982b
Tomonori-T <noritt3397@gmail.com>
2021-05-23 11:33:10 +0000

    cart途中経過

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,36 +1,41 @@
 class CartProductsController < ApplicationController
-  before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
+  before_action :cart_product_params, only: [:create, :update, :destroy]
 
-  def show
-    @cart_items = current_cart.cart_items
+  def index
+    @cart_products = current_cart.cart_items
   end
 
 # 商品一覧画面から、「商品購入」を押した時のアクション
-  def add_item
-    if @cart_item.blank?
-      @cart_item = current_cart.cart_items.build(product_id: params[:product_id])
+  def create
+    if @cart_product.blank?
+      @cart_product = current_cart.cart_items.build(product_id: params[:product_id])
     end
 
-    @cart_item.quantity += params[:quantity].to_i
-    @cart_item.save
-    redirect_to current_cart
+    @cart_product.quantity += params[:quantity].to_i
+    @cart_product.save
+    redirect_to public_cart_products_path
   end
 
 # カート詳細画面から、「更新」を押した時のアクション
-  def update_item
-    @cart_item.update(quantity: params[:quantity].to_i)
-    redirect_to current_cart
+  def update
+    @cart_product.update(quantity: params[:quantity].to_i)
+    redirect_to public_cart_products_path
   end
 
 # カート詳細画面から、「削除」を押した時のアクション
-  def delete_item
-    @cart_item.destroy
-    redirect_to current_cart
+  def destroy
+    @cart_product.destroy
+    redirect_to public_cart_products_path
+  end
+
+  def all_destroy
+    @cart_products.destroy
+    redirect_to public_cart_products_path
   end
 
   private
 
-  def setup_cart_item!
-    @cart_item = current_cart.cart_items.find_by(product_id: params[:product_id])
+  def cart_product_params
+    params.require(:product_id).parmit(:member_id, :product_id, :quantity)
   end
 end
