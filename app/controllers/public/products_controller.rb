efcf3ceddefc96a18d2d2cbae16ee5fbe2ed7127
shota-hibino <yamoushou@gmail.com>
2021-05-24 00:18:14 +0000

    [Update]product layout fixing

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,15 +1,13 @@
 class Public::ProductsController < ApplicationController
-  before_action :authenticate_member!, only: [:show]
-
 	def index
-    @genres = Genre.all
-    @products = Product.where(sale_status: true).page(params[:page]).per(8)
+      @genres = Genre.all
+      @products = Product.where(sale_status: true).page(params[:page]).per(8)
 	end
 
 	def show
-    @products = Product.all
-    @product = Product.find(params[:id])
-    @cart_products = Cart_product.new
+      @products = Product.all
+      @product = Product.find(params[:id])
+      @cart_products = Cart_product.new
 	end
 
   def about
@@ -17,7 +15,7 @@ class Public::ProductsController < ApplicationController
 
 	private
 	def product_params
-		parmas.require(:product).permit(:image_id ,:name, :description, :price, :sale_status)
+	  parmas.require(:product).permit(:image_id ,:name, :description, :price, :sale_status)
 	end
 
 end
