8e09832e7ae70d55d020a817ece5b1a492c73f2e
Tomonori-T <noritt3397@gmail.com>
2021-05-26 13:06:27 +0000

    Schemaファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_105705) do
+ActiveRecord::Schema.define(version: 2021_05_26_125819) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_105705) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_valid", default: false
   end
 
   create_table "members", force: :cascade do |t|
