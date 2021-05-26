0572966071b23649ba98d59cf82ee3699598ce81
Tomonori-T <noritt3397@gmail.com>
2021-05-26 12:49:06 +0000

    退会機能完成版

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2021_05_26_105705) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_valid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
