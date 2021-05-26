a1b8e559b45afb7903c7388b771d1443969e8ce0
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-05-26 12:34:23 +0000

    [Update]

--- app/models/orders_detail.rb
+++ app/models/orders_detail.rb
@@ -5,5 +5,5 @@ class OrdersDetail < ApplicationRecord
   validates :product_id, :order_id, :quantity, :price, presence: true
   validates :price, :quantity, numericality: { only_integer: true }
 
-  enum production_status: {"着手不可": 0,"制作待ち": 1,"作成中": 2,"制作完了": 3 }
+  enum production_status: {"着手不可": 0,"制作待ち": 1,"製作中": 2,"制作完了": 3 }
 end
