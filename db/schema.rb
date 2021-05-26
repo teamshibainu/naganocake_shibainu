97ced4434538844ee91b35f28c446b5925f39289
shota-hibino <yamoushou@gmail.com>
2021-05-26 13:23:03 +0000

    [update]layout fix

--- db/schema.rb
+++ db/schema.rb
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2021_05_26_125819) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.boolean "is_valid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_valid", default: false
   end
 
   create_table "members", force: :cascade do |t|
