2e409521c668898578caf45f4d40df4682f4812f
Tomonori-T <noritt3397@gmail.com>
2021-05-27 09:03:11 +0000

    [Fix] header

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-  email: 'admin@icloud.com',
-  password: "aaaaaa"
-)
+#Admin.create!(
+ # email: 'admin@icloud.com',
+ # password: "aaaaaa"
+#)
 
 Genre.create!(
     name: "ケーキ",
